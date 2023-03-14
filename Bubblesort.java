bde581012603fb4aad05ba8c61dc24006d667afe
MaomaoYsr <116988749+MaomaoYsr@users.noreply.github.com>
2023-03-14 20:01:09 +0000

    Update Bubblesort.java

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,38 +1,45 @@
-//version 1 update 2023-03-14
-import java.util.Scanner;
+//version 1.1 update 2023-03-14
 import java.util.Arrays;
-//冒泡排序算法 此算法通过每一次将该数组中最大的数移到数组的末尾以此类推第二大的数移动到[arr.length-2]以此类推
-//平均时间复杂度为O(n^2) ,最好情况下是O（n） eg （5 1 2 3 4）， 最坏情况是倒序 eg（5 4 3 2 1） 此时时间复杂度为O（n^2)
+import java.util.Scanner;
+
+// Bubble sort algorithm. This algorithm moves the largest number in the array to the end of the array during each iteration, and so on.
+// The average time complexity is O(n^2), and the best case scenario is O(n) (e.g., 5 1 2 3 4). The worst case scenario is reverse order,
+// e.g., 5 4 3 2 1, with a time complexity of O(n^2).
+
 public class Bubblesort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("请输入数组的长度：");
+        System.out.print("Enter the length of the array: ");
         int len = sc.nextInt();
         int[] arr = new int[len];
-        System.out.println("请输入数组的元素：");
+        System.out.println("Enter the elements of the array: ");
         for (int i = 0; i < len; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("冒泡排序前的数组：" + Arrays.toString(arr));
+        System.out.println("Array before bubble sort: " + Arrays.toString(arr));
 
         int count = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
+        boolean flag = true; // Flag to check if any swaps were made
+        for (int i = 0; i < arr.length - 1 && flag; i++) {
+            flag = false;
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     count++;
-                    System.out.println("因为 " + arr[j] + " < " + arr[j + 1] + " ，所以第" + (j + 1) + "个位置和第" + (j + 2) + "个位置交换了位置");
-                    System.out.println("当前数组：" + Arrays.toString(arr));
+                    flag = true; // Swap made, flag set to true
+                    System.out.println("Because " + arr[j] + " < " + arr[j + 1] + ", the elements at position " + (j + 1) + " and " + (j + 2) + " were swapped.");
+                    System.out.println("Current array: " + Arrays.toString(arr));
                 } else {
-                    System.out.println("因为 " + arr[j] + " < || = " + arr[j + 1] + " ，所以第" + (j + 1) + "个位置和第" + (j + 2) + "个位置没有交换位置");
-                    System.out.println("当前数组：" + Arrays.toString(arr));
+                    System.out.println("Because " + arr[j] + " < or = " + arr[j + 1] + ", the elements at position " + (j + 1) + " and " + (j + 2) + " were not swapped.");
+                    System.out.println("Current array: " + Arrays.toString(arr));
                 }
             }
-            System.out.printf("第%d轮排序结果：%s\n", i + 1, Arrays.toString(arr));
+            System.out.printf("Result after round %d of sorting: %s\n", i + 1, Arrays.toString(arr));
         }
-        System.out.println("冒泡排序后的数组：" + Arrays.toString(arr));
-        System.out.println("总共进行了" + count + "次交换");
+        System.out.println("Array after bubble sort: " + Arrays.toString(arr));
+        System.out.println("Total swaps made: " + count);
     }
 }
+
